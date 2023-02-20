56d06006af875a9cfc7fef1cc8dc0a733724fc51
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 00:49:49 +0100

    Añado atributos,constructores y getters and setters de la clase Pintura

--- src/main/java/practica2/Pintura.java
+++ src/main/java/practica2/Pintura.java
@@ -1,5 +1,31 @@
 package practica2;
 
-public class Pintura {
-    
+public class Pintura extends ObraDeArte {
+
+    private String tecnica;
+
+    public Pintura() {
+
+    }
+
+    public Pintura(int id, String nombre, String autor, String tipo, double precio, double altura, double peso,
+            int piezas, String descripcion, String tecnica) {
+        super(id, nombre, autor, tipo, precio, altura, peso, piezas, descripcion);
+        this.tecnica = tecnica;
+    }
+
+    public Pintura(int id, String nombre, String autor, double precio, double altura, double peso,
+            int piezas, String descripcion, String tecnica) {
+        super(id, nombre, autor, precio, altura, peso, piezas, descripcion);
+        this.tecnica = tecnica;
+    }
+
+    public String getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        this.tecnica = tecnica;
+    }
+
 }
