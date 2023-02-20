11e1252ce38e30e997febab41d058d7ecaf6d595
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 00:44:33 +0100

    Añado atributos de ObraDeArte

--- src/main/java/practica2/ObraDeArte.java
+++ src/main/java/practica2/ObraDeArte.java
@@ -1,5 +1,15 @@
 package practica2;
 
 public class ObraDeArte {
-    
+    private int id;
+    private String nombre;
+    private String autor;
+    private String tipo;
+    private String seleccionarTipo[] = { "Pintura", "Escultura" };
+    private double precio;
+    private double altura;
+    private double peso;
+    private int piezas;
+    private String descripcion;
+
 }
