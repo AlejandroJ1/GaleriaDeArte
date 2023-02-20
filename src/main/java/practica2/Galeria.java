7ab3a999448bbf5f3733fdfce10b0f5d5d42b472
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 00:56:56 +0100

    Añado algunos imports, los atributos y algunos getter y setters que necesito em la clase Galeria

--- src/main/java/practica2/Galeria.java
+++ src/main/java/practica2/Galeria.java
@@ -1,5 +1,28 @@
 package practica2;
 
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Galeria {
-    
+
+    Scanner leer = new Scanner(System.in);
+    private Menu menu = new Menu();
+    private ObraDeArte[] obras = new ObraDeArte[5];
+
+    public Scanner getLeer() {
+        return leer;
+    }
+
+    public void setLeer(Scanner leer) {
+        this.leer = leer;
+    }
+
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public ObraDeArte[] getObras() {
+        return obras;
+    }
 }
