9c978e8b22587c9047893056fdeda864a0440062
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 00:47:05 +0100

    Añado Constructores,Getters and Setters de ObraDeArte

--- src/main/java/practica2/ObraDeArte.java
+++ src/main/java/practica2/ObraDeArte.java
@@ -12,4 +12,109 @@ public class ObraDeArte {
     private int piezas;
     private String descripcion;
 
+    public ObraDeArte() {
+
+    }
+
+    public ObraDeArte(int id, String nombre, String autor, String tipo, double precio, double altura, double peso,
+            int piezas, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.tipo = tipo;
+        this.precio = precio;
+        this.altura = altura;
+        this.peso = peso;
+        this.piezas = piezas;
+        this.descripcion = descripcion;
+    }
+
+    public ObraDeArte(int id, String nombre, String autor, double precio, double altura, double peso,
+            int piezas, String descripcion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.autor = autor;
+        this.precio = precio;
+        this.altura = altura;
+        this.peso = peso;
+        this.piezas = piezas;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setSTipo(int tipo) {
+        this.tipo = seleccionarTipo[tipo - 1];
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public int getPiezas() {
+        return piezas;
+    }
+
+    public void setPiezas(int piezas) {
+        this.piezas = piezas;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
 }
