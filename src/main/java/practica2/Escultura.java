80a7c3e642dfca41041620f64e56504b92bb7bd0
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 00:51:26 +0100

    Añado atributos,constructores,getters and setters de la clase Escultura

--- src/main/java/practica2/Escultura.java
+++ src/main/java/practica2/Escultura.java
@@ -1,5 +1,31 @@
 package practica2;
 
-public class Escultura {
-    
+public class Escultura extends ObraDeArte {
+
+    private String material;
+
+    public Escultura() {
+
+    }
+
+    public Escultura(int id, String nombre, String autor, String tipo, double precio, double altura, double peso,
+            int piezas, String descripcion, String material) {
+        super(id, nombre, autor, tipo, precio, altura, peso, piezas, descripcion);
+        this.material = material;
+    }
+
+    public Escultura(int id, String nombre, String autor, double precio, double altura, double peso,
+            int piezas, String descripcion, String material) {
+        super(id, nombre, autor, precio, altura, peso, piezas, descripcion);
+        this.material = material;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
 }
