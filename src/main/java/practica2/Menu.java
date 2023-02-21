ae964382209df1996c9f8a183f8ea2c1f527dbec
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 01:13:18 +0100

    Añado atributos, constructores y el main a menu

--- src/main/java/practica2/Menu.java
+++ src/main/java/practica2/Menu.java
@@ -1,5 +1,23 @@
 package practica2;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Menu {
-    
+
+    Scanner leer = new Scanner(System.in);
+    private static Galeria galeria = new Galeria();
+
+    public Menu(Scanner leer) {
+        this.leer = leer;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public Menu() {
+
+    }
+
 }
