f1af6b093e5398c1f7553481f6e0a5a68ec280c1
AlejandroJ1 <alejandro.jimenezmartin@tiernoparla.es>
2023-02-21 01:15:35 +0100

    Agrego la base del Menu Principal

--- src/main/java/practica2/Menu.java
+++ src/main/java/practica2/Menu.java
@@ -20,4 +20,65 @@ public class Menu {
 
     }
 
+    public static void MenuPrincipal(ObraDeArte[] obras) {
+
+        int opcion;
+        boolean finalizar = false;
+
+        while (!finalizar) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Bienvenido a nuestra galería de arte");
+            System.out.println("(1) Visualizar las obras de arte de la galería");
+            System.out.println("(2) Dar de alta una obra de arte");
+            System.out.println("(3) Modificar datos de una obra");
+            System.out.println("(4) Visualiar datos de una obra de arte");
+            System.out.println("(5) Obtener el precio de venta de una de las obras");
+            System.out.println("(6) Dar información adcinional sobre una obra");
+            opcion = sc.nextInt();
+            try {
+                switch (opcion) {
+                    case 1:
+                        sc.nextLine();
+                        System.out.println("Has seleccionadp la 1");
+                        
+                        sc.nextLine();
+                        break;
+                    case 2:
+                        System.out.println("Has seleccionado la 2");
+                        sc.nextLine();
+                        
+                        sc.nextLine();
+                        break;
+                    case 3:
+                        System.out.println("Has seleccionado la 3");
+                        
+                        break;
+                    case 4:
+                        System.out.println("Has seleccionado la 4");
+
+                        break;
+                    case 5:
+                        System.out.println("Has seleccionado la 5");
+                        
+                        break;
+                    case 6:
+                        System.out.println("Has seleccionado la 6");
+
+                        break;
+                    case 0:
+                        System.out.println(finalizar);
+                        break;
+                    default:
+                        System.out.println("Numeros del 1 al 6, por favor.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Debes insertar un número");
+                sc.next();
+            }
+            System.out.println("¡¡Gracias por usar esta aplicación!!");
+           
+        }
+
+    }
+
 }
